19c4a6fd94af1b4df4b83bdc8602fa57ee7a1c1a
Ross Loney <majoross@gmail.com>
2021-05-26 13:55:25 -0500

    createPhoneNumber (codewars)

---
+++ codewars/createPhoneNumber.js
@@ -0,0 +1,11 @@
+const createPhoneNumber = (numArr) => {
+  // extract area code
+  const areaCode = numArr.slice(0, 3).join('');
+  // extract first three digits
+  const firstThree = numArr.slice(3, 6).join('');
+  // extract last four digits
+  const lastFour = numArr.slice(6).join('');
+  return `(${areaCode}) ${firstThree}-${lastFour}`;
+};
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // "(123) 456-7890"
